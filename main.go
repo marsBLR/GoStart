c78b0dfd84684d0021e6900473ebefef53e13ada
rozhkov.sergei <rozhkov.sergei.2014@gmail.com>
2020-06-21 21:28:29 +0300

    part 1

--- main.go
+++ main.go
@@ -1,42 +1,98 @@
 package main
 
 import (
+	"blog-Go_SR/models"
+	"blog-Go_SR/utils"
 	"fmt"
 	"html/template"
 	"net/http"
 )
 
-//var posts map[string]*Post
+var posts map[string]*models.Post
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
-
+		return
 	}
 
-	t.ExecuteTemplate(w, "index", nil)
+	fmt.Println(posts)
+
+	for index, value := range posts {
+		fmt.Println(index, value)
+	}
+	t.ExecuteTemplate(w, "index", posts)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	id := r.FormValue("id")
+	post, found := posts[id]
+	if !found {
+		http.NotFound(w, r)
+		return
+	}
+
+	t.ExecuteTemplate(w, "write", post)
 }
 
 func writeHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/write.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
-
+		return
 	}
 
 	t.ExecuteTemplate(w, "write", nil)
 }
 
-//
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	if id == "" {
+		http.NotFound(w, r)
+	}
+
+	delete(posts, id)
+
+	http.Redirect(w, r, "/", 302)
+}
+
+func savePostHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.FormValue("id")
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+
+	var post *models.Post
+	if id != "" {
+		post = posts[id]
+		post.Title = title
+		post.Content = content
+	} else {
+		fmt.Println("next func Generate")
+		id = utils.GenerateId()
+		post := models.NewPost(id, title, content)
+		posts[post.Id] = post
+	}
+
+	http.Redirect(w, r, "/", 302)
+}
 
 func main() {
 	fmt.Println("Listening on port :3001")
-	//	posts = make(map[string]*Post, 0)
+
+	posts = make(map[string]*models.Post, 0)
+
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/write", writeHandler)
-	//http.HandleFunc("/SavePost", savePostHandler)
+	http.HandleFunc("/edit", editHandler)
+	http.HandleFunc("/delete", deleteHandler)
+	http.HandleFunc("/SavePost", savePostHandler)
 
 	http.ListenAndServe(":3001", nil)
 }
